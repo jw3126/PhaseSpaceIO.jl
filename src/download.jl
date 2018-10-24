c5bc2fbe14caf7d45746c387a477bcb7f58dea8b
Jan Weidner <jw3126@gmail.com>
2018-10-24 10:55:51 +0200

    fix

--- src/download.jl
+++ src/download.jl
@@ -4,8 +4,7 @@ import JSON
 const IAEA_PHSP_URLS = JSON.parsefile(
     joinpath(@__DIR__, "phsplinks.json"))
 
-function iaea_download_path(name)
-    dir = abspath(joinpath(@__DIR__, "..", "download"))
+function iaea_download_path(dir,name)
     IAEAPath(joinpath(dir, name))
 end
 
@@ -18,8 +17,15 @@ function _download(url, path, reload)
     end
 end
 
-function iaea_download(name, path::IAEAPath=iaea_download_path(name);
+function iaea_download(name;
+                       dir = nothing,
+                       path = nothing,
                       reload=false)
+    @argcheck dir == nothing || path == nothing
+    @argcheck dir != nothing || path != nothing
+    if path == nothing
+        path = iaea_download_path(dir, name);
+    end
     urls = IAEA_PHSP_URLS[name]
     mkpath(splitdir(path.header)[1])
     mkpath(splitdir(path.phsp)[1])
