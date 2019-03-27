1ba4fe0d2a14c2b673572223bb7ae8d1ee86dca5
Jan Weidner <jw3126@gmail.com>
2019-03-27 13:37:36 +0100

    add position, direction getters and setters

--- test/test_getters.jl
+++ test/test_getters.jl
@@ -4,6 +4,7 @@ using PhaseSpaceIO
 using PhaseSpaceIO.Testing
 using PhaseSpaceIO.Getters
 using Setfield
+using StaticArrays
 
 @testset "test getters" begin
     p = arbitrary(IAEAParticle{0,1})
@@ -22,6 +23,33 @@ using Setfield
     @test p.zlast == nothing
     p = @set p.zlast = 1f0
     @test p.zlast == 1f0
+
+    dir = StaticArrays.normalize(@SVector(randn(3)))
+    pos_iaea = @SVector(randn(3))
+    pos_egs = @SVector(randn(2))
+    for (p, pos, dir) in [
+                          (arbitrary(EGSParticle{Float32}), pos_egs, dir),
+                          (arbitrary(IAEAParticle{2,3}), pos_iaea, dir),
+           ]
+        q = set_position(p, pos)
+        @test typeof(q) == typeof(p)
+        @test q.E === p.E
+        @test position(q) ≈ pos
+        
+        q = set_direction(p, dir)
+        @test typeof(q) == typeof(p)
+        @test q.E === p.E
+
+        @test direction(q) ≈ dir
+        @test dir ≈ [q.u, q.v, q.w]
+    end
+
+    p = arbitrary(EGSParticle{Nothing})
+    @test position(p) == [p.x, p.y]
+    @test position(p,z=3) == [p.x, p.y, 3]
+
+    p = arbitrary(IAEAParticle{1,2})
+    @test position(p) == [p.x, p.y, p.z]
 end
 
 end
