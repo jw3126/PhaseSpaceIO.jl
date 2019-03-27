a8d0136ecd779510b9e8f4c86e17f51c118bf0ee
Jan Weidner <jw3126@gmail.com>
2019-03-27 18:37:40 +0100

    use Requires

--- src/PhaseSpaceIO.jl
+++ src/PhaseSpaceIO.jl
@@ -2,6 +2,7 @@ __precompile__()
 module PhaseSpaceIO
 using ArgCheck
 using DataStructures
+using Requires
 
 include("abstract.jl")
 include("iohelpers.jl")
@@ -15,4 +16,13 @@ include("testing.jl")
 include("deprecate.jl")
 include("download.jl")
 
+function __init__()
+    @require StaticArrays="90137ffa-7385-5640-81b9-e52037218182" begin
+        include("staticarrays.jl")
+        @require CoordinateTransformations="150eb455-5306-5404-9cee-2592286d6298" begin
+            include("transforms.jl")
+        end
+    end
 end
+
+end#module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+include("test_optional.jl")
 include("test_api.jl")
 include("test_iohelpers.jl")
 include("test_egs.jl")
@@ -5,4 +6,3 @@ include("test_iaea.jl")
 include("test_core.jl")
 include("test_getters.jl")
 include("test_experimental.jl")
-
