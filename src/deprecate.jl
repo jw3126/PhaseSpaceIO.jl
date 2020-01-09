2d8a32283d8b50d52b5e9109070fbad3eb1bffa5
Jan Weidner <jw3126@gmail.com>
2020-01-09 04:27:40 -0800

    remove deprecations

--- src/deprecate.jl
+++
@@ -1,8 +0,0 @@
-@deprecate phsp_writer(args...)   iaea_writer(args...)
-@deprecate Particle IAEAParticle
-@deprecate PhaseSpaceIterator IAEAPhspIterator
-@deprecate PhaseSpaceWriter IAEAPhspWriter
-function Base.convert(::Type{ParticleType}, i::Integer) 
-    @warn "convert(::Type{ParticleType}, i::Integer) is deprecated. Use ParticleType(i) instead"
-    ParticleType(i)
-end
